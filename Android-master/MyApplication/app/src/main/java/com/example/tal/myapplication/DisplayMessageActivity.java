7bd54b4846420530f8bd254d93a3833e40d94818
Rene <risanche@ucsd.edu>
2015-11-02 11:33:23 -0800

    Added comments to DisplayMessage.java

--- Android-master/MyApplication/app/src/main/java/com/example/tal/myapplication/DisplayMessageActivity.java
+++ Android-master/MyApplication/app/src/main/java/com/example/tal/myapplication/DisplayMessageActivity.java
@@ -25,7 +25,7 @@ public class DisplayMessageActivity extends ActionBarActivity{
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_display_message);
 
-        //Get the items from the intent
+        //Get the classes inputted by the user from the intent
         Intent intent = getIntent();
         final ArrayList<String> classList = intent.getStringArrayListExtra(MainActivity.EXTRA_CLASSLIST);
         curr_num_classes = intent.getIntExtra(MainActivity.EXTRA_NUM, DEFAULT_CLASS_NUM);
@@ -34,40 +34,49 @@ public class DisplayMessageActivity extends ActionBarActivity{
         final TextView textView = new TextView(this);
         textView.setTextSize(30);
 
+        //Thread that does all of the CAPE querying and string printing
        Thread getCapeData = new Thread(new Runnable(){
            @Override
            public void run(){
                final Document doc;
                try{
+                   //Create the URL that displays the cape URL that contains the first user-unputted class
                    String capeURL =  "http://www.cape.ucsd.edu/responses/Results.aspx?CourseNumber=" + classList.get(0);
+
+                   //Connect to the website
                    doc = Jsoup.connect(capeURL)
                            .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                            .referrer("http://www.google.com")
                            .get();
 
+                       //create another thread that will run on the UI - NEEDED
                        runOnUiThread(new Runnable(){
                            @Override
                            public void run(){
-                           Element table = doc.select("table").get(0); //select the first table.
+                           Element table = doc.select("table").get(0); //select the first (only) table.
                            Elements rows = table.select("tr");
-                           ArrayList<String> workHours = new ArrayList<String>();
+                           ArrayList<String> workHours = new ArrayList<String>(); //ArrayList that can store the hours expected
 
-                           for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
+                           for (int i = 1; i < rows.size(); i++) {    //first row is the col names so skip it.
                                Element row = rows.get(i);
                                Elements cols = row.select("td");
-                               workHours.add(cols.get(7).text());
+                               workHours.add(cols.get(7).text());     //column 7 of the table is the work hrs column
                            }
 
+                               //Create a string to display to the screen.
+                               //Display the classes inputted by the user
                                String setText = "CLASSES: ";
                                for (int i=0; i<curr_num_classes; i++){
                                    setText = setText + "\nClass "+ (i+1) + ": " + classList.get(i);
                                }
 
+                               //Append the work hours for the first class to the string
                                setText = setText + "\nHours for first class: " + workHours.get(0);
 
+                               //Store the string we just created into the textView object
                            textView.setText(setText);
 
-                           // Set the text view as the activity layout
+                           // Set the text view as the activity layout (print the string to the screen)
                            setContentView(textView);
                            }
                        });
@@ -79,6 +88,7 @@ public class DisplayMessageActivity extends ActionBarActivity{
            }
        });
 
+        //Function that starts the above thread
         getCapeData.start();
 
 /*
